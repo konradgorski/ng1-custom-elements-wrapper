35ecd1c637d8a6658eb2a050ecb94d46a0dfeeba
Pal, Siddh <Siddharth.pal@ADP.com>
2019-02-23 23:48:16 +0530

    feat: change @Input as name and @Output as name-change

--- elements/app/app.js
+++ elements/app/app.js
@@ -1,9 +1,13 @@
 (function () {
   var app = angular.module('hello-world-app', ['ng1WebComponents']);
   app.component('greetUser', {
-    template: '<greet-ce-wrapper message="$ctrl.message"></greet-ce-wrapper>',
+    template: '<greet-ce-wrapper name="$ctrl.name" name-change="$ctrl.nameChange(detail)"></greet-ce-wrapper>',
     controller: function GreetUserController() {
-      this.message = 'Welcome to AngularJs upgrade with Angular Elements';
+      this.name = ' Erin Coughlan';
+
+      this.nameChange = function(detail) {
+        this.name =  detail;
+      }
     }
   });
 })();
--- libs/greet/src/lib/greet/greet.component.ts
+++ libs/greet/src/lib/greet/greet.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'ng1-ce-wrapper-greet',
@@ -6,8 +6,20 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./greet.component.scss']
 })
 export class GreetComponent implements OnInit {
-  @Input() message: String;
+  @Input() name: String;
+  @Output() nameChange = new EventEmitter();
+
   constructor() {}
 
+  updateName() {
+    let name = '';
+
+    if (this.name === 'Erin Coughlan') {
+      name = 'Siddharth Pal';
+    } else {
+      name = 'Erin Coughlan';
+    }
+    this.nameChange.emit(name);
+  }
   ngOnInit() {}
 }
